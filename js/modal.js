e657a710f4267adf8431509c01480dd8731dc4aa
TatyanaZdota <99191661+TatyanaZdota@users.noreply.github.com>
2022-07-16 00:04:49 +0300

    add mobile header

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector(".header-button"),
+    closeModalBtn: document.querySelector(".header-close-button"),
+    modal: document.querySelector(".mob-menu"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
